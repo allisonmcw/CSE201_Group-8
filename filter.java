ab7796916a2a4b852d0abbf19dd790e6c59d6e2b
phamav <phamav@miamioh.edu>
2021-11-04 20:12:08 -0400

    Create filter.java
    Filtering methods.
    Use HashMap.

---
+++ filter.java
@@ -0,0 +1,46 @@
+/**
+ * File 4: Filtering method
+ * Author: Jenn Pham
+ */
+
+import static org.junit.Assert.assertEquals;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+import org.junit.Test;
+
+public class filter {
+    public static Map<String, String> filterCategory(Map<String, String> m) {
+        Map<String, String> m1 = new HashMap<String, String>();
+        m1.put("Candy Crush", "Game");
+        m1.put("Roblox", "Game");
+        m1.put("Canvas", "Education");
+        m1.put("Instagram", "Social Media");
+
+        // Filter call for game category
+        Map<String, String> filteredMap = m1.entrySet().stream().filter(x -> "Game".equals(x.getValue())).collect(Collectors.toMap(Map.Entry::getKey, Map.Entry::getValue));
+
+        return filteredMap;
+   }
+
+
+   	/**
+	 * Tester 3: Filtering method()
+	 */
+   @Test
+   public void testCategory() {
+       Map<String, String> m1 = new HashMap<String, String>();
+       m1.put("Candy Crush", "Game");
+       m1.put("Roblox", "Game");
+       m1.put("Canvas", "Education");
+       m1.put("Instagram", "Social Media");
+
+       m1 = filter.filterCategory(m1);
+       
+       assertEquals("Game", m1.get("Candy Crush"));
+    //    assertEquals("Education", m1.get("Canvas"));
+   }
+
+}
