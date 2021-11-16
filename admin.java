3c04bc620f304bf3f021aba1591845ec95bf6df4
allisonmcw <91986498+allisonmcw@users.noreply.github.com>
2021-11-16 14:24:05 -0500

    Update admin.java

--- admin.java
+++ admin.java
@@ -15,6 +15,9 @@ import java.io.FileNotFoundException;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Scanner;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
 
 import javax.swing.*;
 
@@ -57,78 +60,189 @@ public class admin {
         n1.setBounds(50, 100, 200, 30);
         a.add(n1);
         c1 = new JLabel("Description");
-        c1.setBounds(150, 100, 200, 30);
+        c1.setBounds(175, 100, 200, 30);
         a.add(c1);
         c1 = new JLabel("Platform");
-        c1.setBounds(250, 100, 200, 30);
+        c1.setBounds(300, 100, 200, 30);
         a.add(c1);
         c1 = new JLabel("Versions");
-        c1.setBounds(350, 100, 200, 30);
-        a.add(c1);
-        c1 = new JLabel("Store Link");
-        c1.setBounds(450, 100, 200, 30);
+        c1.setBounds(425, 100, 200, 30);
         a.add(c1);
         c1 = new JLabel("Price");
         c1.setBounds(550, 100, 200, 30);
         a.add(c1);
         c1 = new JLabel("Category");
-        c1.setBounds(650, 100, 200, 30);
+        c1.setBounds(675, 100, 200, 30);
         a.add(c1);
         c1 = new JLabel("Store Name");
-        c1.setBounds(750, 100, 200, 30);
+        c1.setBounds(800, 100, 200, 30);
         a.add(c1);
-        JTextField b7 = new JTextField("App 1");
-        a.add(b7);
-        b7.setBounds(50, 200, 200, 30);
-        clickClear(b7);
+        c1 = new JLabel("Store Link");
+        c1.setBounds(925, 100, 200, 30);
+        a.add(c1);
+        JTextField name = new JTextField("Name");
+        a.add(name);
+        name.setBounds(50, 150, 100, 30);
+        clickClear(name);
+        JTextField desc = new JTextField("Description");
+        a.add(desc);
+        desc.setBounds(175, 150, 100, 30);
+        clickClear(desc);
+        JTextField plat = new JTextField("Platform");
+        a.add(plat);
+        plat.setBounds(300, 150, 100, 30);
+        clickClear(plat);
+        JTextField ver = new JTextField("Versions");
+        a.add(ver);
+        ver.setBounds(425, 150, 100, 30);
+        clickClear(ver);
+        JTextField price = new JTextField("Price");
+        a.add(price);
+        price.setBounds(550, 150, 100, 30);
+        clickClear(price);
+        JTextField cat = new JTextField("Category");
+        a.add(cat);
+        cat.setBounds(675, 150, 100, 30);
+        clickClear(cat);
+        JTextField storeName = new JTextField("Store Name");
+        a.add(storeName);
+        storeName.setBounds(800, 150, 100, 30);
+        clickClear(storeName);
+        
+        JPopupMenu b13 = new JPopupMenu("Store Name");
+        a.add(b13);
+        // Categories: games, streaming, music, social media, education
+        // Store Names: Google Play, Apple Store, Microsoft Store
+        JMenuItem goog = new JMenuItem("Google Play");  
+        JMenuItem appl = new JMenuItem("Apple");  
+        JMenuItem micro = new JMenuItem("Microsoft");  
+        b13.add(goog); b13.add(appl); b13.add(micro);        
+        storeName.addMouseListener(new MouseAdapter() {  
+           public void mouseClicked(MouseEvent e) {              
+               b13.show(a , 800, 150);  
+           }                 
+        });  
+        goog.addActionListener(new ActionListener(){  
+            public void actionPerformed(ActionEvent e) {              
+            	storeName.setText("Google Play");
+            }  
+           });  
+           appl.addActionListener(new ActionListener(){  
+               public void actionPerformed(ActionEvent e) {              
+            	   storeName.setText("Apple");  
+               }  
+              });  
+           micro.addActionListener(new ActionListener(){  
+               public void actionPerformed(ActionEvent e) {              
+            	   storeName.setText("Microsoft");   
+               }  
+              });  
+        b13.setBounds(800, 150, 100, 30);
+//        clickClear(b13);
+        // Categories: games, streaming, music, social media, education
+        JPopupMenu b12a = new JPopupMenu("Category");
+        a.add(b12a);
+        JMenuItem game = new JMenuItem("Games");  
+        JMenuItem stream = new JMenuItem("Streaming");  
+        JMenuItem music = new JMenuItem("Music");  
+        JMenuItem soc = new JMenuItem("Social Media");  
+        JMenuItem edu = new JMenuItem("Education");  
+        b12a.add(game); b12a.add(stream); b12a.add(music); b12a.add(soc); b12a.add(edu);        
+        cat.addMouseListener(new MouseAdapter() {  
+           public void mouseClicked(MouseEvent e) {              
+               b12a.show(a , 675, 150);  
+           }                 
+        });
+        game.addActionListener(new ActionListener(){  
+            public void actionPerformed(ActionEvent e) {              
+            	cat.setText("Games");
+            }  
+           });  
+           stream.addActionListener(new ActionListener(){  
+               public void actionPerformed(ActionEvent e) {              
+            	   cat.setText("Streaming");  
+               }  
+              });  
+           music.addActionListener(new ActionListener(){  
+               public void actionPerformed(ActionEvent e) {              
+            	   cat.setText("Music");   
+               }  
+              });  
+           soc.addActionListener(new ActionListener(){  
+               public void actionPerformed(ActionEvent e) {              
+            	   cat.setText("Social Media");   
+               }  
+              });  
+           edu.addActionListener(new ActionListener(){  
+               public void actionPerformed(ActionEvent e) {              
+            	   cat.setText("Education");   
+               }  
+              });  
+        
+        b12a.setBounds(675, 150, 100, 30);
+        
+        JTextField b14 = new JTextField("Store Link");
+        a.add(b14);
+        b14.setBounds(925, 150, 100, 30);
+        clickClear(b14);
+        
+        String toFile = name.getText() + "," + desc.getText() + "," + plat.getText() + "," + ver.getText() + "," +
+        		price.getText() + "," + cat.getText() + "," + storeName.getText();
+        
 
-        JTextField b8 = new JTextField("App 2");
-        a.add(b8);
-        b8.setBounds(50, 250, 200, 30);
-        clickClear(b8);
-
-        // JLabel c2;
+        JLabel c2;
         JButton d1 = new JButton("Approve");
-        d1.setBounds(275, 200, 100, 30);
+        d1.setBounds(1050, 150, 100, 30);
         a.add(d1);
         
-        // JLabel c3;
-        JButton d2 = new JButton("Approve");
-        d2.setBounds(275, 250, 100, 30);
-        a.add(d2);
-//        c2 = new JLabel("Create Account");
-//        c2.setBounds(275, 50, 200, 20);
-//        a.add(c2);
-//        JTextField b2 = new JTextField("First Name");
-//        a.add(b2);
-//        b2.setBounds(275, 100, 200, 30);
-//        clickClear(b2);
-//
-//        JTextField b3 = new JTextField("Last Name");
-//        a.add(b3);
-//        b3.setBounds(275, 150, 200, 30);
-//        clickClear(b3);
-//
-//        JTextField b4 = new JTextField("Email");
-//        a.add(b4);
-//        b4.setBounds(275, 200, 200, 30);
-//        clickClear(b4);
-//
-//        JTextField b5 = new JTextField("Password");
-//        a.add(b5);
-//        b5.setBounds(275, 250, 200, 30);
-//        clickClear(b5);
-//
-//        JTextField b6 = new JTextField("Re-type Password");
-//        a.add(b6);
-//        b6.setBounds(275, 300, 200, 30);
-//        clickClear(b6);
-//
-//        JButton d = new JButton("Register");
-//        d.setBounds(275, 350, 100, 25);
-//        a.add(d);
-//        a.setBounds(10, 10, 500, 500);
 
+        // Add app
+        d1.addActionListener((ActionListener) new ActionListener() {
+
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                HashMap<String, String> map = new HashMap<String, String>();
+                map = readFromFile();
+
+//                System.out.println("Iterating Hashmap...");
+//                for (Map.Entry<String, String> entry : map.entrySet()) {
+//                    String key = entry.getKey();
+//                    String value = entry.getValue();
+//                    System.out.println(key+" "+value);
+//                    // ...
+//                }
+                boolean addApp = false;
+                for (Map.Entry<String, String> entry : map.entrySet()) {
+                    String key = entry.getKey();
+                    String value = entry.getValue();
+                    if (key.compareTo(storeName.getText()) == 0) {
+                        if (value.compareTo(storeName.getText().trim()) == 0) {
+                            // System.out.println("Successful Login");
+                            JOptionPane.showMessageDialog(null,
+                                    "App Added!");
+                            addApp = true;
+                            // GUI gu = new GUI();
+//                            GUI.logInAfter(key);
+                        } else {
+                            // System.out.println("Expecting: "+b8.getText()+"
+                            // Got: "+value);
+                            // System.out.println("Wrong Password");
+                            JOptionPane.showMessageDialog(null,
+                                    "App Unsuccessful");
+                            addApp = true;
+                        }
+                    }
+
+                    // ...
+                }
+                if (!addApp)
+                    // System.out.println("Wrong email");
+                    JOptionPane.showMessageDialog(null, "Incorrect Information");
+                storeName.setText("");
+                storeName.setText("");
+            }
+
+        });
 
     } // end main
 
@@ -146,7 +260,7 @@ public class admin {
         HashMap<String, String> map = new HashMap<String, String>();
         try {
             Scanner sc = new Scanner(new File(
-                    "Accounts.txt"));
+                    "Apps.txt"));
             while (sc.hasNext()) {
                 String line = sc.nextLine();
                 String[] attributes = line.split(",");
